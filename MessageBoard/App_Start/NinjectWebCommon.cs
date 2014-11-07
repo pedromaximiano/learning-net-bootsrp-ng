3df914a013fc719101dcb20ba3d1ef822b33bf65
pedromaximiano <pedro.maximiano@gmail.com>
2014-11-07 11:49:06 +0000

    Prototypes the UI. Adds styles.

--- MessageBoard/App_Start/NinjectWebCommon.cs
+++ MessageBoard/App_Start/NinjectWebCommon.cs
@@ -8,9 +8,7 @@ namespace MessageBoard.App_Start
 {
     using System;
     using System.Web;
-
     using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-
     using Ninject;
     using Ninject.Web.Common;
 
