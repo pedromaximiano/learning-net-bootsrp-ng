7725a05382bc4a50a200700b0fd53ba66aa09611
pedromaximiano <pedro.maximiano@gmail.com>
2014-11-10 16:14:55 +0000

    Adds an AngularJS controller.

---
+++ MessageBoard/js/home-index.js
@@ -0,0 +1,21 @@
+﻿// home-index.js
+var angularFormsApp = angular.module("messageBoardApp", []);
+
+angularFormsApp.controller("homeIndexController", function homeIndexController($scope, $http) {
+    $scope.dataCount = 0;
+    $scope.data = [];
+    $scope.isBusy = true;
+
+    $http.get("/api/v1/topics?includeReplies=true").then(
+        function (result) {
+            // Success
+            angular.copy(result.data, $scope.data);
+            $scope.dataCount = result.data.length;
+        },
+        function () {
+            // Error
+            alert("could not load topics");
+        }).then(function () {
+            $scope.isBusy = false;
+        });
+});
