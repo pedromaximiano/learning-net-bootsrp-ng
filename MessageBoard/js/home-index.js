02b71ec0f3ee61465cf3ee6531d18ba5c9cdb2b9
pedromaximiano <pedro.maximiano@gmail.com>
2014-11-11 18:53:29 +0000

    Adds and shows how to use a custom service.

--- MessageBoard/js/home-index.js
+++ MessageBoard/js/home-index.js
@@ -15,34 +15,85 @@ angularFormsApp.config(function ($routeProvider) {
     $routeProvider.otherwise({ redirectTo: "/" });
 });
 
-angularFormsApp.controller("topicsController", function topicsController($scope, $http) {
-    $scope.dataCount = 0;
-    $scope.data = [];
-    $scope.isBusy = true;
+angularFormsApp.factory("dataService", function ($http, $q) {
+    var _topics = [];
+    var _isInit = false;
 
-    $http.get("/api/v1/topics?includeReplies=true").then(
-        function (result) {
-            // Success
-            angular.copy(result.data, $scope.data);
-            $scope.dataCount = result.data.length;
-        },
-        function () {
-            // Error
-            alert("could not load topics");
-        }).then(function () {
-            $scope.isBusy = false;
-        });
-});
+    var _isReady = function () {
+        return _isInit;
+    };
 
-angularFormsApp.controller("newTopicController", function newTopicController($scope, $http, $window) {
-    $scope.newTopic = {};
-    $scope.save = function () {
-        $http.post("/api/v1/topics", $scope.newTopic).then(
+    var _getTopics = function () {
+        var deferred = $q.defer();
+
+        $http.get("/api/v1/topics?includeReplies=true").then(
             function (result) {
                 // Success
-                var newTopic = result.data;
-                
-                //TODO: merge
+                angular.copy(result.data, _topics);
+                _isInit = true;
+                deferred.resolve();
+            },
+            function () {
+                // Error
+                deferred.reject();
+            });
+
+        return deferred.promise;
+    };
+
+    var _addTopic = function (newTopic) {
+        var deferred = $q.defer();
+
+        $http.post("/api/v1/topics", newTopic).then(
+            function (result) {
+                // Success
+                var createdTopic = result.data;
+
+                _topics.splice(0, 0, createdTopic);
+                deferred.resolve(createdTopic);
+            }, function () {
+                // Error
+                deferred.reject();
+            });
+
+        return deferred.promise;
+    };
+
+
+    return {
+        topics: _topics,
+        getTopics: _getTopics,
+        addTopic: _addTopic,
+        isReady: _isReady
+    };
+});
+
+angularFormsApp.controller("topicsController", function topicsController($scope, $http, dataService) {
+    $scope.dataCount = 0;
+    $scope.data = dataService;
+    $scope.isBusy = false;
+
+    if (dataService.isReady() == false) {
+        $scope.isBusy = true;
+        dataService.getTopics().then(
+            function () {
+                // Success
+            },
+            function () {
+                // Error
+                alert("could not load topics");
+            }).then(function () {
+                $scope.isBusy = false;
+            });
+    }
+});
+
+angularFormsApp.controller("newTopicController", function newTopicController($scope, $http, $window, dataService) {
+    $scope.newTopic = {};
+    $scope.save = function () {
+        dataService.addTopic($scope.newTopic).then(
+            function () {
+                // Success
                 $window.location = "#/";
             }, function () {
                 // Error
